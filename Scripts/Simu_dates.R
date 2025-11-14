57f8b00c44c465eb650c1a4090a8aa4c34aaae2f
acortell3 <alfredo.cortell.nicolau@gmail.com>
2025-11-14 17:39:11 +0100

    Simu dates with actual values

--- Scripts/Simu_dates.R
+++ Scripts/Simu_dates.R
@@ -9,7 +9,9 @@ library(rcarbon)
 ## Set pars for different simulations
 C14_errors <- c(20,50)
 r <- c(0.01,0.02,0.03,0.04)
-start_date <- sample(1000:45000, 100 , replace = F)
+
+########## Younger dates HOLOCENE
+start_date <- sample(1000:11000, 1000, replace = F)
 ndates <- 80
 i_row <- 1 # To update df row index
 
@@ -28,10 +30,39 @@ for (i in 1:length(start_date)){
 			simuls[c(i_row:(i_row+ndates-1)),] <- c(YearsBP[1],YearsBP[2],as.data.frame(rep(start_date[i],ndates)),as.data.frame(rep(r[j],ndates)))
 			i_row <- 1+nrow(simuls) ## Update the row to store the information
     			# Need to save it like this for CWIRM
-			write.table(YearsBP,file = paste0("../Simu_data/Uncal_YearsBP_sim_",i,"_r_",r[j],"_error_",C14_errors[k],".txt"), sep = "\t", row.names = F) ## It purposedly overwrites the above
+			write.table(YearsBP,file = paste0("../Simu_data/Uncal_YearsBP_sim_hol_",i,"_r_",r[j],"_error_",C14_errors[k],".txt"), sep = "\t", row.names = F) ## It purposedly overwrites the above
 		}
 	}
 }
 
-saveRDS(simuls,"../Simu_data/simuls.rds")
+saveRDS(simuls,"../Simu_data/simuls_hol.rds")
+
+##### OLDER UPPER PALAEOLITHIC
+
+## Set pars for different simulations
+start_date <- sample(30000:40000, 1000, replace = F)
+i_row <- 1 # To update df row index
+
+## Create data frame to store simulations
+simuls <- data.frame("YearsBP" = NA,
+		     "Sd" = NA,
+		     "start_date" = NA,
+		     "r" = NA)
+## Generate dates
+for (i in 1:length(start_date)){
+	for (j in 1:length(r)){
+		for (k in 1:length(C14_errors)){
+			modelPlot(dLogisticGrowth,a=start_date[i],b=start_date[i]-1000,params=c(k=0.1,r=r[j]),alpha=1,col='black') #the sampling distribution where k is the proportion of K (carrying capacity).
+			x <- replicate(ndates,rLogisticGrowth(a=start_date[i],b=start_date[i]-1000,k=0.1,r=r[j])) #sample 80 dates from the distribution above in calendar time (cannot use n=80 as nimble is unhappy with that)
+			YearsBP <- uncalibrate(x,CRAerrors=C14_errors[k])[,c('rCRA','rError')]
+			simuls[c(i_row:(i_row+ndates-1)),] <- c(YearsBP[1],YearsBP[2],as.data.frame(rep(start_date[i],ndates)),as.data.frame(rep(r[j],ndates)))
+			i_row <- 1+nrow(simuls) ## Update the row to store the information
+    			# Need to save it like this for CWIRM
+			write.table(YearsBP,file = paste0("../Simu_data/Uncal_YearsBP_sim_up_",i,"_r_",r[j],"_error_",C14_errors[k],".txt"), sep = "\t", row.names = F) ## It purposedly overwrites the above
+		}
+	}
+}
+
+saveRDS(simuls,"../Simu_data/simuls_up.rds")
+
 
