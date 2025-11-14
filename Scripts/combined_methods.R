6bdf7ebe5b8b68ad54359ba79b1dc8ee99cdf34d
acortell3 <alfredo.cortell.nicolau@gmail.com>
2025-11-14 11:47:14 +0100

    First draft combined script done

--- Scripts/combined_methods.R
+++ Scripts/combined_methods.R
@@ -6,6 +6,7 @@ library(rcarbon)
 library(oxcAAR) 
 library(stringr)
 library(Rextinct)
+library(parallel)
 
 ## Oxcal setup
 quickSetupOxcal() #this should be replaced by actual reference to the file path where oxcal is downloaded (otherwise would install it every time)
@@ -13,26 +14,6 @@ quickSetupOxcal() #this should be replaced by actual reference to the file path 
 ## Load required functions
 source('oxcal_functions.R')
 source('functions.R')
-
-## Load data
-
-
-## Need this to recover files by name. They match the ones in Simu_dates.R
-#sim <- 100
-#C14_errors <- c(20,50)
-#r <- c(0.01,0.02,0.03,0.04)
-
-#for (h in 1:nfiles){
-#	for (i in 1:sim){
-#		for (k in 1:length(r)){
-#			for (j in 1:length(C14_errors)){
-#				dates  <- readRDS(paste0('../Simu_data/Uncal_YearsBP_sim_',i,'_r_',r[k],'_error_',C14_errors[j],'.rds'))
-#				colnames(dates) <- c("Year","Sd")
-#				dates_text  <- read.table(paste0('../Simu_data/Uncal_YearsBP_sim_',i,'_r_',r[k],'_error_',C14_errors[j],'.txt'))
-#			}
-#		}
-#	}
-#}
 
 ## Load data and utilities
 dates <- readRDS("../Simu_data/simuls.rds")
@@ -75,21 +56,21 @@ for (z in 1:10){
 })
 
 
-######### OLE RESAMPLE
+######### OLE RESAMPLE CALDATE (Our proposal)
 ## Ok, so what they do in (Key et al. 2021, Bebber & Key, 2022 or Djakovic et al., 2022) is basically resample from the mean of the radiocarbon date with half standard deviation. They sample 10,000 times and from the results, the just get the mean of the estimated means and CIs. I'll do the last bit (although it is dodgy), but I'm not resampling from the radiocarbon date because this does not take into account the calibration error. I'll sample from the probability distribution of the calibrated date. 
 
+rsmp <- 1000 ## Number of resamples. Shared between the two OLE resampling methods
 
 ## Prepare to store results
-OLE_resamp <- data.frame("Estimate" = numeric(),
-	        	 "upperCI" = numeric(),
-			 "lowerCI" = numeric(),
-			 "start_date" = numeric(),
-			 "r" = numeric(),
-			 "sd" = numeric(),
-			 "sampled_dates" = numeric())
+OLE_resamp_caldate <- data.frame("Estimate" = numeric(),
+	        	 	 "upperCI" = numeric(),
+			 	 "lowerCI" = numeric(),
+			 	 "start_date" = numeric(),
+			 	 "r" = numeric(),
+			 	 "sd" = numeric(),
+			 	 "sampled_dates" = numeric())
 
 
-rsmp <- 1000 ## Number of resamples
 system.time({
 for (z in 1:40){
 	## Temporal OLE results
@@ -126,22 +107,133 @@ for (z in 1:40){
 		}
 	}
 	
-	OLE_resamp[z,] <- c(apply(temp_OLE,2,mean),dates_subset$start_date[1],dates_subset$r[1],dates_subset$Sd[1],dates_ss[z])
+	OLE_resamp_caldate[z,] <- c(apply(temp_OLE,2,mean),dates_subset$start_date[1],dates_subset$r[1],dates_subset$Sd[1],dates_ss[z])
+}
+})
+
+######### OLE RESAMPLE NORM DIST (as in Key et al., 2021 and Bebber and Key 2022)
+## Ok, so what they do in (Key et al. 2021, Bebber & Key, 2022 or Djakovic et al., 2022) is basically resample from the mean of the radiocarbon date with half standard deviation. They sample 10,000 times and from the results, the just get the mean of the estimated means and CIs. I'll do the last bit (although it is dodgy), but I'm not resampling from the radiocarbon date because this does not take into account the calibration error. I'll sample from the probability distribution of the calibrated date. 
+
+
+## Prepare to store results
+OLE_resamp_norm <- data.frame("Estimate" = numeric(),
+	                      "upperCI" = numeric(),
+			      "lowerCI" = numeric(),
+			      "start_date" = numeric(),
+			      "r" = numeric(),
+			      "sd" = numeric(),
+			      "sampled_dates" = numeric())
+
+system.time({
+for (z in 1:40){
+	## Temporal OLE results
+	temp_OLE <- data.frame("Estimate" = numeric(),
+			       "upperCI" = numeric(),
+		       	       "lowerCI" = numeric())
+	
+	dates_subset <- dates[(ndates*z-79):(ndates*z),]
+	
+	# Calibrate dates
+	cal_dates <- calibrate(dates_subset[,1],dates_subset[,2])
+	
+	for (i in 1:rsmp){
+		valid <- F ## I'll use this to check whether the matrix is singluar
+		
+		while(!valid){
+			# Resample
+			resamp_dates <- rep(NA,ndates)
+			for (k in 1:ndates){
+				dat_mean <- mean(cal_dates$grids[[k]][,1])
+				dat_sd <- (max(cal_dates$grids[[k]][,1])-dat_mean)/2
+				resamp_dates[k] <- rnorm(1,dat_mean,dat_sd)
+			}
+			resamp_dates <- resamp_dates[order(resamp_dates, decreasing = T)]
+			
+			for (j in 1:length(dates_ss)){
+				resamp_dates <- resamp_dates[1:dates_ss[j]]
+			
+				## Check that matrix is singlular and produce results only if it is
+				OLE_res <- OLE.test(dd = resamp_dates, alpha = 0.05)
+		
+				if (!any(is.na(OLE_res))){
+					temp_OLE[i,] <- OLE_res
+					valid <- TRUE
+				}
+			}
+		}
+	}
+	
+	OLE_resamp_norm[z,] <- c(apply(temp_OLE,2,mean),dates_subset$start_date[1],dates_subset$r[1],dates_subset$Sd[1],dates_ss[z])
+}
+})
+
+######### OLE RESAMPLE UNI DIST (as in Djakovic et al. 2022)
+## Ok, so what they do in (Key et al. 2021, Bebber & Key, 2022 or Djakovic et al., 2022) is basically resample from the mean of the radiocarbon date with half standard deviation. They sample 10,000 times and from the results, the just get the mean of the estimated means and CIs. I'll do the last bit (although it is dodgy), but I'm not resampling from the radiocarbon date because this does not take into account the calibration error. I'll sample from the probability distribution of the calibrated date. 
+
+
+## Prepare to store results
+OLE_resamp_uni <- data.frame("Estimate" = numeric(),
+	                     "upperCI" = numeric(),
+			     "lowerCI" = numeric(),
+			     "start_date" = numeric(),
+			     "r" = numeric(),
+			     "sd" = numeric(),
+			     "sampled_dates" = numeric())
+
+system.time({
+for (z in 1:40){
+	## Temporal OLE results
+	temp_OLE <- data.frame("Estimate" = numeric(),
+			       "upperCI" = numeric(),
+		       	       "lowerCI" = numeric())
+	
+	dates_subset <- dates[(ndates*z-79):(ndates*z),]
+	
+	# Calibrate dates
+	cal_dates <- calibrate(dates_subset[,1],dates_subset[,2])
+	
+	for (i in 1:rsmp){
+		valid <- F ## I'll use this to check whether the matrix is singluar
+		
+		while(!valid){
+			# Resample
+			resamp_dates <- rep(NA,ndates)
+			for (k in 1:ndates){
+				resamp_dates[k] <- runif(1,min(cal_dates$grids[[k]][,1]),max(cal_dates$grids[[k]][,1]))
+			}
+			resamp_dates <- resamp_dates[order(resamp_dates, decreasing = T)]
+			
+			for (j in 1:length(dates_ss)){
+				resamp_dates <- resamp_dates[1:dates_ss[j]]
+			
+				## Check that matrix is singlular and produce results only if it is
+				OLE_res <- OLE.test(dd = resamp_dates, alpha = 0.05)
+		
+				if (!any(is.na(OLE_res))){
+					temp_OLE[i,] <- OLE_res
+					valid <- TRUE
+				}
+			}
+		}
+	}
+	
+	OLE_resamp_uni[z,] <- c(apply(temp_OLE,2,mean),dates_subset$start_date[1],dates_subset$r[1],dates_subset$Sd[1],dates_ss[z])
 }
 })
 
 ######### Oxcal
-unif_res <- list()
-trap_res <- list()
 
-for (z in 1:1){
-dates_subset <- dates[(ndates*z-79):(ndates*z),]
+# Cores
+ncores <- 10
 
-unif_res[[z]]  <- oxcalRunner(c14age=round(dates_subset$Year),error=dates_subset$Sd,model='uniform')
-trap_res_temp[[z]]  <- oxcalRunner(c14age=round(dates_subset$Year),error=dates_subset$Sd,model='trapezoid')
-
-
-}
+system.time({
+oxcal_res <- mclapply(1:10, function(z) {
+			      dates_subset <- dates[(ndates*z-79):(ndates*z), ]
+			      list(unif  = oxcalRunner(c14age = round(dates_subset$Year),error  = dates_subset$Sd,model  = "uniform"),
+				   trap  = oxcalRunner(c14age = round(dates_subset$Year),error  = dates_subset$Sd,model  = "trapezoid")
+			      )
+			       }, mc.cores = ncores)
+})
 
 ######### CRIWM
 
