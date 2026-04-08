fdbef258681e2bf6f806872e1a148625bb756df9
acortell3 <alfredo.cortell.nicolau@gmail.com>
2026-04-08 18:36:54 +0200

    update figures

--- Scripts/Results.R
+++ Scripts/Results.R
@@ -116,12 +116,12 @@ colorinchis <- c("darkorange2","darkorange3","darkorange4")
 par(mfrow=c(3,2),mar=c(10,4,2,2))
 for (i in 1:length(index_r)){
 	for (j in 1:length(index_Sd)){
-		plot(NULL,xlim=c(1,pl_num),ylim=c(0,7500),axes=F,xlab='',ylab='Precision')
-		abline(h = seq(0,7000,100), lty = 2, col = adjustcolor("gray", alpha = 0.3))
+		plot(NULL,xlim=c(1,pl_num),ylim=c(0,9750),axes=F,xlab='',ylab='Precision')
+		abline(h = seq(0,9000,100), lty = 2, col = adjustcolor("gray", alpha = 0.3))
 		title(main=paste0('r=',index_r[i],', Sd=',index_Sd[j]))
 		counter  <- 1
 		for (k in 1:length(index_sample_size)){
-			text(x=counter+4,y=7450,paste('sample size=',index_sample_size[k]),cex=1,pos=2)
+			text(x=counter+4,y=9700,paste('sample size=',index_sample_size[k]),cex=1,pos=2)
 			for (m in 1:length(index_method)){
 				ii  <- which(hol_data$r==index_r[i]&hol_data$Sd==index_Sd[j]&hol_data$sample_size==index_sample_size[k]&hol_data$method==index_method[m])
 				if(length(ii)>1){
@@ -277,17 +277,18 @@ colorinchis <- c("darkorange2","darkorange3","darkorange4")
 par(mfrow=c(3,2),mar=c(10,4,2,2))
 for (i in 1:length(index_r)){
 	for (j in 1:length(index_Sd)){
-		plot(NULL,xlim=c(1,pl_num),ylim=c(0,6500),axes=F,xlab='',ylab='Precision')
-		abline(h = seq(0,6000,100), lty = 2, col = adjustcolor("gray", alpha = 0.3))
+		plot(NULL,xlim=c(1,pl_num),ylim=c(0,11500),axes=F,xlab='',ylab='Precision')
+		abline(h = seq(0,10000,100), lty = 2, col = adjustcolor("gray", alpha = 0.3))
 		title(main=paste0('r=',index_r[i],', Sd=',index_Sd[j]))
 		counter  <- 1
 		for (k in 1:length(index_sample_size)){
-			text(x=counter+4,y=6350,paste('sample size=',index_sample_size[k]),cex=1,pos=2)
+			text(x=counter+4,y=11450,paste('sample size=',index_sample_size[k]),cex=1,pos=2)
 			for (m in 1:length(index_method)){
 				ii  <- which(ple_data$r==index_r[i]&ple_data$Sd==index_Sd[j]&ple_data$sample_size==index_sample_size[k]&ple_data$method==index_method[m])
 				if(length(ii)>1){
 					tmp <- ple_data[ii,]
-					boxplot(tmp$precision,at=counter,add=T,outline=F,axes=F,lty=1,col = colorinchis[k])
+					vioplot(tmp$precision, at = counter, add = T, outline = F, axes = F, lty =1 , col = colorinchis[k])
+					#boxplot(tmp$precision,at=counter,add=T,outline=F,axes=F,lty=1,col = colorinchis[k])
 					axis(1,at=counter,label=index_method[m],las=2,cex=0.5)
 					counter  <- counter+1
 				}
