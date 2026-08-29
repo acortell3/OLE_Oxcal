a653a3b2dd08fa99472d0827729457ce6c5140c1
acortell3 <alfredo.cortell.nicolau@gmail.com>
2026-08-29 19:11:26 +0200

    added distances to UFO

--- R/04_Data_output.R
+++ R/04_Data_output.R
@@ -34,10 +34,27 @@ OLE_resamp_unif_hol <- OLE_resamp_unif_hol[OLE_resamp_unif_hol$sample_size %in% 
 
 CRIWM_hol$overallAgreementIndex <- OLE_medians_hol$overallAgreementIndex <- OLE_resamp_caldate_hol$overallAgreementIndex <- OLE_resamp_norm_hol$overallAgreementIndex <- OLE_resamp_unif_hol$overallAgreementIndex <- NA
 
+CRIWM_hol$dist_to_UFO <- OLE_medians_hol$dist_to_UFO <- OLE_resamp_caldate_hol$dist_to_UFO <- OLE_resamp_norm_hol$dist_to_UFO <- OLE_resamp_unif_hol$dist_to_UFO <- NA
+
+CRIWM_hol$ranked_position <- OLE_medians_hol$ranked_position <- OLE_resamp_caldate_hol$ranked_position <- OLE_resamp_norm_hol$ranked_position <- OLE_resamp_unif_hol$ranked_position <- NA
+
 ## Builds oxcal dfs
 ## Some Oxcal results failed with "Error in BCAD(post.df[,2]: 0 BC/AD is not a valid year". Subset only the ones for which the time range was valid
 oxcal_trap_hol <- oxcal_trap_hol[sapply(oxcal_trap_hol,length)>1]
 oxcal_unif_hol <- oxcal_unif_hol[sapply(oxcal_unif_hol,length)>1]
+
+## Function to find the years to the UFO of the date with the lowest Agreement index
+dist_agr <- function(x){
+	return(abs(x$start_date - x$individualAgreement$CRA[which.min(x$individualAgreement$agreement)]))
+}
+
+## Function to find, from the ordered dates, where does the date with the lowest agreement rank
+rank_agr <- function(x){
+	x$individualAgreement$dist_to_start <- abs(x$start_date - x$individualAgreement$CRA)
+	## Order according to distance to UFO
+	ordf <- x$individualAgreement[order(x$individualAgreement$dist_to_start),]
+	return(which.min(ordf$agreement))
+}
 
 ## Oxca dfs
 oxcal_trap_hol_df <- data.frame("Estimate" = rep(NA,length(oxcal_trap_hol)),
@@ -49,7 +66,9 @@ oxcal_trap_hol_df <- data.frame("Estimate" = rep(NA,length(oxcal_trap_hol)),
 				"ESS" = sapply(oxcal_trap_hol, function(x) x$ESS),
 				"sample_size" = sapply(oxcal_trap_hol, function(x) x$ESS),
 				"simID_seed" = rep(NA,length(oxcal_trap_hol)),
-				"overallAgreementIndex" = sapply(oxcal_trap_hol, function(x) x$overallAgreement))
+				"overallAgreementIndex" = sapply(oxcal_trap_hol, function(x) x$overallAgreement),
+				"dist_to_UFO" = sapply(oxcal_trap_hol, dist_agr),
+				"ranked_position" = sapply(oxcal_trap_hol, rank_agr))
 
 oxcal_unif_hol_df <- data.frame("Estimate" = rep(NA,length(oxcal_unif_hol)),
 				"upperCI" = sapply(oxcal_unif_hol, function(x) x$posteriorRange[1,1]),
@@ -60,7 +79,9 @@ oxcal_unif_hol_df <- data.frame("Estimate" = rep(NA,length(oxcal_unif_hol)),
 				"ESS" = sapply(oxcal_unif_hol, function(x) x$ESS),
 				"sample_size" = sapply(oxcal_unif_hol, function(x) x$ESS),
 				"simID_seed" = rep(NA,length(oxcal_unif_hol)),
-				"overallAgreementIndex" = sapply(oxcal_unif_hol, function(x) x$overallAgreement))
+				"overallAgreementIndex" = sapply(oxcal_unif_hol, function(x) x$overallAgreement),
+				"dist_to_UFO" = sapply(oxcal_unif_hol,dist_agr),
+				"ranked_position" = sapply(oxcal_unif_hol,rank_agr))
 
 hol_df <- rbind(CRIWM_hol,OLE_medians_hol,OLE_resamp_caldate_hol,OLE_resamp_norm_hol,OLE_resamp_unif_hol,oxcal_trap_hol_df,oxcal_unif_hol_df)
 
@@ -86,6 +107,10 @@ oxcal_unif_upl <- readRDS(paste0(path,"oxcal_unif_upl.rds"))
 
 CRIWM_upl$overallAgreementIndex <- OLE_medians_upl$overallAgreementIndex <- OLE_resamp_caldate_upl$overallAgreementIndex <- OLE_resamp_norm_upl$overallAgreementIndex <- OLE_resamp_unif_upl$overallAgreementIndex <- NA
 
+CRIWM_upl$dist_to_UFO <- OLE_medians_upl$dist_to_UFO <- OLE_resamp_caldate_upl$dist_to_UFO <- OLE_resamp_norm_upl$dist_to_UFO <- OLE_resamp_unif_upl$dist_to_UFO <- NA
+
+CRIWM_upl$ranked_position <- OLE_medians_upl$ranked_position <- OLE_resamp_caldate_upl$ranked_position <- OLE_resamp_norm_upl$ranked_position <- OLE_resamp_unif_upl$ranked_position <- NA
+
 ## Builds oxcal dfs
 ## Oxca dfs
 oxcal_trap_upl_df <- data.frame("Estimate" = rep(NA,length(oxcal_trap_upl)),
@@ -97,7 +122,9 @@ oxcal_trap_upl_df <- data.frame("Estimate" = rep(NA,length(oxcal_trap_upl)),
 				"ESS" = sapply(oxcal_trap_upl, function(x) x$ESS),
 				"sample_size" = sapply(oxcal_trap_upl, function(x) x$ESS),
 				"simID_seed" = rep(NA,length(oxcal_trap_upl)),
-				"overallAgreementIndex" = sapply(oxcal_trap_upl, function(x) x$overallAgreement))
+				"overallAgreementIndex" = sapply(oxcal_trap_upl, function(x) x$overallAgreement),
+				"dist_to_UFO" = sapply(oxcal_trap_upl,dist_agr),
+				"ranked_position" = sapply(oxcal_trap_upl,rank_agr))
 
 oxcal_unif_upl_df <- data.frame("Estimate" = rep(NA,length(oxcal_unif_upl)),
 				"upperCI" = sapply(oxcal_unif_upl, function(x) x$posteriorRange[1,1]),
@@ -108,7 +135,9 @@ oxcal_unif_upl_df <- data.frame("Estimate" = rep(NA,length(oxcal_unif_upl)),
 				"ESS" = sapply(oxcal_unif_upl, function(x) x$ESS),
 				"sample_size" = sapply(oxcal_unif_upl, function(x) x$ESS),
 				"simID_seed" = rep(NA,length(oxcal_unif_upl)),
-				"overallAgreementIndex" = sapply(oxcal_unif_upl, function(x) x$overallAgreement))
+				"overallAgreementIndex" = sapply(oxcal_unif_upl, function(x) x$overallAgreement),
+				"dist_to_UFO" = sapply(oxcal_unif_upl,dist_agr),
+				"ranked_position" = sapply(oxcal_unif_upl,rank_agr))
 
 upl_df <- rbind(CRIWM_upl,OLE_medians_upl,OLE_resamp_caldate_upl,OLE_resamp_norm_upl,OLE_resamp_unif_upl,oxcal_trap_upl_df,oxcal_unif_upl_df)
 
